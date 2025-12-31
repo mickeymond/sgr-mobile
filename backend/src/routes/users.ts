348e211e815bfd7dc75d324bc6e78ff8941a1122
Michael Hammond <33749130+mickeymond@users.noreply.github.com>
2025-12-31 12:16:38 +0000

    feat: Add user login and profile retrieval routes.

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
+import basicAuth from "express-basic-auth";
 import { body, matchedData, validationResult } from "express-validator";
 import { connection } from "../utils/db";
+import { myAsyncAuthorizer } from "../middlewares/auth";
 
 // Initialize router
 const usersRouter: Router = Router();
 
-// Routes
+// User login
 usersRouter.post(
   "/login",
   body("username").isString(),
@@ -30,6 +32,27 @@ usersRouter.post(
     }
 
     res.json({ success: true, data: result[0], error: null });
+  },
+);
+
+// Get user profile
+usersRouter.get(
+  "/me",
+  basicAuth({
+    authorizer: myAsyncAuthorizer,
+    authorizeAsync: true,
+  }),
+  async (req, res) => {
+    const { user, password } = (req as basicAuth.IBasicAuthedRequest).auth;
+
+    const users: Array<any> = await connection.query(
+      `SELECT TOP 1 * FROM usertable WHERE user_name = '${user}' AND [password] = '${password}';`,
+    );
+    if (!users.length) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    res.json(users[0]);
   },
 );
 
